f4d3eabd0e33c211c498575dab39dd14352e3313
rids3d <137489131+pwsecurity@users.noreply.github.com>
2025-06-29 07:08:43 -0400

    Update script.js

--- script.js
+++ script.js
@@ -63,7 +63,7 @@ const users = {
   nihaj3d: [
     { ip: "https://drive.google.com/drive/folders/1RG_C7VNh6a8WPOH-ojtnm_b-h3CVmg3Y?usp=sharing", location: "Drive Link" }
   ],
-  g: [
+  jubaer3d: [
     { ip: "67.217.243.238:10580:ca24fd14ds:cgh5dgfv", location: "California 2 NODE" },
     { ip: "67.217.244.5:9050:ny054czvrfd:cs46f4wef", location: "New York  NODE" },
     { ip: "                 18 March 2025               ", location: "Birth" },
@@ -432,8 +432,8 @@ function downloadZipFile() {
     case 'nihaj3d':
       zipFileName = 'Nihaj.zip';
       break;
-    case 'g':
-      zipFileName = 'g.zip';
+    case 'jubaer3d':
+      zipFileName = 'Zubaer.zip';
       break;
     case 'g':
       zipFileName = 'g.zip';
